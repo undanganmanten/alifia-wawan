8bc127c7a6ad18b46aecbdf25953181e243d24fa
Undangan Manten <68999804+undanganmanten@users.noreply.github.com>
2021-12-10 00:48:51 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -82,6 +82,25 @@ var x = setInterval(function() {
 }, 1000);
 
 
+//kirim hadiah
+let modal = document.querySelector(".modal");
+let trigger = document.querySelector(".trigger");
+let closeButton = document.querySelector(".close-button");
+
+function toggleModal() {
+    modal.classList.toggle("show-modal");
+}
+
+function windowOnClick(event) {
+    if (event.target === modal) {
+        toggleModal();
+    }
+}
+
+trigger.addEventListener("click", toggleModal);
+closeButton.addEventListener("click", toggleModal);
+window.addEventListener("click", windowOnClick);
+
 // firebase
 var firebaseConfig = {
   apiKey: "AIzaSyAh9V_jR3OhD3_GnViXdb-HXMfaZ3KdVjA",
